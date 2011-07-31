d2ac33a3d479dece14ed0ef0206645fcc6e41be3
delano <delano@solutious.com>
2011-07-30 21:12:14 -0400

    Added User#name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_filter :login_required, :except => [:new, :create]
   # FIXME need admin logins
 
+  def index
+    @users = User.find(:all)
+  end
+
   def new
     @user = User.new
   end
@@ -19,16 +23,32 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
-  def update
-    @user = current_user
-    if @user.update_attributes(params[:user])
-      redirect_to root_url, :notice => "Your profile has been updated."
-    else
-      render :action => 'edit'
-    end
-  end
+  # def update
+  #   @user = current_user
+  #   if @user.update_attributes(params[:user])
+  #     redirect_to root_url, :notice => "Your profile has been updated."
+  #   else
+  #     render :action => 'edit'
+  #   end
+  # end
   
   def show
     @user = current_user
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :notice  => "Successfully updated user."
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url, :notice => "Successfully destroyed user."
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110608000129) do
+ActiveRecord::Schema.define(:version => 20110731010147) do
 
   create_table "capacities", :force => true do |t|
     t.integer  "specialist_id"
@@ -67,6 +67,9 @@ ActiveRecord::Schema.define(:version => 20110608000129) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "direct_phone"
+    t.string   "moa_name"
+    t.string   "moa_phone"
+    t.string   "moa_email"
   end
 
   create_table "specializations", :force => true do |t|
@@ -83,6 +86,7 @@ ActiveRecord::Schema.define(:version => 20110608000129) do
     t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
