220e3c3525a5230ddc54ea0753d5df98c6b22cca
Kalin Harvey <kalin@gmail.com>
2011-08-09 20:04:17 -0700

    added join tables for specialists + clinics/hospitals

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,6 +1,9 @@
 class Clinic < ActiveRecord::Base
   attr_accessible :name, :address1, :address2, :postalcode, :city, :province, :phone1, :fax, :status, :interest, :waittime, :specialization_id
   
+  has_many :attendances
+  has_many :specialists, :through => :attendances
+  
   validates_presence_of :specialization_id, :on => :create, :message => "can't be blank"
   validates_presence_of :name, :on => :create, :message => "can't be blank"
 end
--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,7 +1,9 @@
 class Procedure < ActiveRecord::Base
   attr_accessible :name, :specialization_id
+
   has_many :capacities
   has_many :specialists, :through => :capacities
+  
   belongs_to :specialization
 
   default_scope :order => 'name ASC'
