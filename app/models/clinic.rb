c4f4cadc90636893c198c8fbd346671c4810a210
delano <delano@solutious.com>
2011-08-10 03:27:51 -0400

    UI: clinic selects specialization from dropdown

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -4,6 +4,8 @@ class Clinic < ActiveRecord::Base
   has_many :attendances
   has_many :specialists, :through => :attendances
   
+  belongs_to :specialization
+  
   validates_presence_of :specialization_id, :on => :create, :message => "can't be blank"
   validates_presence_of :name, :on => :create, :message => "can't be blank"
 end
