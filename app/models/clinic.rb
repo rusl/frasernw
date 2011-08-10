c6fc7426bd327842ce55d4cba2d8a4a60c74a0e8
delano <delano@solutious.com>
2011-08-10 03:19:46 -0400

    UI: Add referral_* to clinic edit page

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,5 +1,5 @@
 class Clinic < ActiveRecord::Base
-  attr_accessible :name, :address1, :address2, :postalcode, :city, :province, :phone1, :fax, :status, :interest, :waittime, :specialization_id
+  attr_accessible :name, :address1, :address2, :postalcode, :city, :province, :phone1, :fax, :status, :interest, :waittime, :specialization_id, :referral_criteria, :referral_process
   
   has_many :attendances
   has_many :specialists, :through => :attendances
