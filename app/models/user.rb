38af1337457106c8a2c9fe8ac5a6ef67ee470770
Kalin Harvey <kalin@gmail.com>
2011-09-07 12:08:54 -0700

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   has_many :favorites
   has_many :specialists, :through => :favorites
   # has_many :clinics,     :through => :favorites
+  validates_presence_of :name
+  validates_presence_of :username
+  validates_presence_of :email
+  
   def admin?
     self.role == 'admin'
   end
