e35ed1c8f8aebc0dffe3966401c6378b6c648f22
Matthew Lehner <matthewlehner@gmail.com>
2011-09-27 06:43:57 +0000

    only mail users on create

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,5 +1,5 @@
 class UserObserver < ActiveRecord::Observer
-  def after_save(user)
+  def after_create(user)
     UserMailer.welcome_admin(user).deliver
     # Notifications.comment("admin@do.com", "New comment was posted", comment).deliver
   end
