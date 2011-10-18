4d6c35cf23ad986841d5e5a8d5e4a156a01274e3
Matthew Lehner <matthewlehner@gmail.com>
2011-10-18 10:59:20 -0700

    Add units of measurement to form and model

--- app/models/specialist.rb
+++ app/models/specialist.rb
@@ -1,5 +1,5 @@
 class Specialist < ActiveRecord::Base
-  attr_accessible :firstname, :lastname, :address1, :address2, :postalcode, :city, :province, :phone1, :fax, :practise_limitations, :interest, :waittime, :specialization_id, :procedure_ids, :direct_phone, :red_flags, :clinic_ids, :responds_via, :contact_name, :contact_email, :contact_phone, :contact_notes, :referral_criteria, :status_mask, :location_opened, :referral_fax, :referral_phone, :referral_other_details, :urgent_fax, :urgent_phone, :urgent_other_details, :respond_by_fax, :respond_by_phone, :respond_by_mail, :respond_to_patient, :status_details, :required_investigations, :not_performed, :lagtime, :patient_can_book, :hospital_ids, :capacities_attributes
+  attr_accessible :firstname, :lastname, :address1, :address2, :postalcode, :city, :province, :phone1, :fax, :practise_limitations, :interest, :waittime, :specialization_id, :procedure_ids, :direct_phone, :red_flags, :clinic_ids, :responds_via, :contact_name, :contact_email, :contact_phone, :contact_notes, :referral_criteria, :status_mask, :location_opened, :referral_fax, :referral_phone, :referral_other_details, :urgent_fax, :urgent_phone, :urgent_other_details, :respond_by_fax, :respond_by_phone, :respond_by_mail, :respond_to_patient, :status_details, :required_investigations, :not_performed, :lagtime, :patient_can_book, :lag_uom, :wait_uom, :hospital_ids, :capacities_attributes
   has_paper_trail
   
   belongs_to :specialization
@@ -32,14 +32,16 @@ class Specialist < ActiveRecord::Base
   validates_presence_of :lastname, :on => :save, :message => "can't be blank"
   validates_presence_of :specialization_id, :on => :save, :message => "can't be blank"
 
+  after_initialize :default_values
+
+  default_scope order('lastname, firstname')
+
   STATUS_HASH = { 1 => "Accepting new patients", 2 => "Only doing follow up on previous patients", 3 => "Semi-retired", 4 => "Retired"}
 
   def status
     Specialist::STATUS_HASH[status_mask]
   end
-  
-  default_scope order('lastname, firstname')
-  
+
   def name
     firstname + ' ' + lastname
   end
@@ -50,11 +52,11 @@ class Specialist < ActiveRecord::Base
     address += self.address2 || ''
     address
   end
-  
+
   def waittime_or_blank
     self.waittime.blank? ? "n/a" : self.waittime
   end
-  
+
   def waittime?
     self.waittime.blank? ? 'muted' : ''
   end
@@ -68,5 +70,13 @@ class Specialist < ActiveRecord::Base
       return self.saved_token
     end
   end
+
+  private
+
+    def default_values
+      lag_uom ||= "weeks"
+      wait_uom ||= "weeks"
+    end
+
 
 end
