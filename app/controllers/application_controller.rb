6ac2493d8723887eac3bbc4b3fb60ff0e435ff31
Matthew Lehner <matthewlehner@gmail.com>
2011-10-25 22:45:04 -0700

    paper_trail default meta info for reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   include ControllerAuthentication
   before_filter :login_required
   protect_from_forgery
+
+  def info_for_paper_trail
+    { :reviewed => (!current_user.admin? rescue true) }
+  end
 end
